65539f6353c68e2f7df1e4e09b3d7ca436f6a2ba
vivek9211 <82077595+vivek9211@users.noreply.github.com>
2023-03-23 19:10:49 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,5 +2,3 @@ export const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country="
 export const API_KEY = "aa16f1e6aa51c18aafd125b81702be00"
 export const endpointPath = (country, category) => `${API_DOMAIN}${country}&category=${category}&apikey=${API_KEY}`;
 
-// f2eb478b0cdb5909364e59fdc321344f
-// "aa16f1e6aa51c18aafd125b81702be00"
